4d5f416912f945fb091736eb0a7500c606d79c4a
AshishDmss <ashishk@dmss.co.in>
2019-02-28 17:44:19 +0530

    update

--- app/src/main/java/common/Common.java
+++ app/src/main/java/common/Common.java
@@ -167,53 +167,7 @@ public static String[]addBusinessLocationKeys={"location_name","street_name",
         "sunday_afternoon_from",
         "sunday_afternoon_to"};
 
-
-
-
-
-
-
-
-
-
-
-
-    //"
-//        /business_products_offers/add
-///business_products_offers/update
-///business_products_offers/delete
-///business_products_offers/details
-///business_products_offers/search"
-
-   //   public static String paypalClientId="Aeotj4cpU2wDvlExN1qZciDC8KP-cG4xR4ZwbJIK7Ec2UGfNzn6_LRIMLz8c0oAY8gTfXaxdUhyJbaup";
-//    public static String paypalClientSecret="EJTAzxAEzeoIrTTfgZkDyQHsZseHNVjUx-RlQco_XETkm6KxLvpPkRF-lViyWjhplXNdXp1d0DeCYBfG";
    public static String paypalClientId="ATEDn8H26GtM_VpLMrIWrTwvJCxc6d_xWNa0W34PL9FqLwEg41FRrjlLZppbhR_ShSG50ztPZ6bpPBXz";
-  //  public static String paypalClientId="AWjXaMC3FMmr70GbAEHKVpCXQQ5XyZ745RQ-yFq_2Vu7E-AEog9xi_XokV0De0CJr2Q_QlWzannKEQzp";
-    public static String paypalClientSecret="EF0a3sqSFCDwlYmpiTGhJNrEapL_qi2Gdktsh5lOhJTtMoxuDdgkjK-BcoKJeznwYrEegLRpHwh5fTgB";
-
-
- //      /business_users/complete_signup_with_paypal
-
-//    "/business_locations
-//            /business_locations/add
-///business_locations/update
-///business_locations/delete
-///business_locations/search
-///business_locations/details"
-
-
-
-//    "/business_products/categories
-//            /business_products
-///business_products/add
-///business_products/update
-///business_products/delete
-///business_products/details
-///business_products/search"
-
-
-
-
-
+   public static String paypalClientSecret="EF0a3sqSFCDwlYmpiTGhJNrEapL_qi2Gdktsh5lOhJTtMoxuDdgkjK-BcoKJeznwYrEegLRpHwh5fTgB";
 
 }
